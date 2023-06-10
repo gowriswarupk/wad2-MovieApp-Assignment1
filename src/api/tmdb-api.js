36cd74329d2dd20ab7e0ae599ca707b2be058fee
Kannan <contactgowri2000@gmail.com>
2023-06-10 17:32:33 +0100

    added pagination, checked functionality.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,6 +1,7 @@
-export const getMovies = () => {
+export const getMovies = (args) => {
+  const [,page] = args.queryKey;
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
   )
     .then((response) => {
       if (!response.ok) {
@@ -137,4 +138,21 @@ export const getActors = () => {
   )
     .then(res => res.json())
     .then(json => json.results);
-};      
+};
+
+export const getActorsDetails = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
