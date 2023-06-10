1254fffd18b9ab4315375c58311df708d5f3635c
Kannan <contactgowri2000@gmail.com>
2023-06-10 16:49:03 +0100

    fixed siteheader error not showing popular actors listing

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -33,7 +33,8 @@ const SiteHeader = ({ history }) => {
     { label: "Top Rated", path: "/movies/topRated" },
     { label: "Now Playing", path: "/movies/nowPlaying" },
     { label: "Trending", path: "/movies/trending" },
-    { label: "Upcoming", path: "/movies/upcoming" }
+    { label: "Upcoming", path: "/movies/upcoming" },
+    { label: "Popular Actors", path: "/popular_actors" }
   ];
 
   const handleMenuSelect = (pageURL) => {
@@ -49,7 +50,7 @@ const SiteHeader = ({ history }) => {
       <AppBar position="fixed" color="secondary">
         <Toolbar>
           <Typography variant="h4" className={classes.title}>
-            TMDB Client
+            NOTFLIX MOVIE DB
           </Typography>
           <Typography variant="h6" className={classes.title}>
             All you ever wanted to know about Movies!
